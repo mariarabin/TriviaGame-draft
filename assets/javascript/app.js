f2797392e7f75f746ee5ea96ef989cd76b929ae0
Maria <lrabin@gmail.com>
2019-04-12 01:16:12 -0400

    initial file

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+// Page loads as a start of the game
+$(document).ready(function () {
+    alert("Let's get R-E-A-D-Y👾!!!");
+
+
+    //======VARIABLES========
+
+    var questions = [
+        {
+            question: 'What is the capital of the Philippines?',
+            answers: [
+                { answer: 'A. Manila', value: true },
+                { answer: 'B. Cebu', value: false },
+                { answer: 'C. Luzon', value: false },
+                { answer: 'D. Metro Manila', value: false }
+            ]
+        },
+        {
+            question: 'How many states are there in the United States of America?',
+            answers: [
+                { answer: 'A. 51', value: false },
+                { answer: 'B. 48', value: true },
+                { answer: 'C. 50', value: false },
+                { answer: 'D. None', value: false }
+            ]
+        }]
+    //=============MAIN PROCESS===================
+
+    //Only one answer is allowed per question
+
+
+
+    //display timer
+
+    //=============FUNCTIONS===================
+
+    //start of the game - start timer
+
+    //End Game - End Timer
+
+    //Reset Game
+
+    //Check Wins - score
+
+    //Check Wronng Answers -- -- reveal answers
+
+
+
+
+
+
+
+});
